88e770d0d19bb267d9462ba0e4375571bca13398
artur-hurynovich <artur.hurynovich@gmail.com>
2023-03-13 12:18:20 +0100

    initial commit

---
+++ src/main/java/com/example/hurynovich/template_filler/TemplateFillerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hurynovich.template_filler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateFillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateFillerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hurynovich/template_filler/TemplateFillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hurynovich.template_filler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateFillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
