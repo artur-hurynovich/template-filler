0041588758a8b6ce0d92091c67df3808a3cb678f
artur-hurynovich <artur.hurynovich@gmail.com>
2023-03-13 12:36:19 +0100

    add repositories

---
+++ src/main/java/com/example/hurynovich/template_filler/repository/TemplateRepository.java
@@ -0,0 +1,8 @@
+package com.example.hurynovich.template_filler.repository;
+
+import com.example.hurynovich.template_filler.entity.TemplateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemplateRepository extends JpaRepository<TemplateEntity, Long> {
+
+}
